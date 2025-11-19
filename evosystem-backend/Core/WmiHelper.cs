2415cf1432d1993ac8a8c40ef013fe78740f7843
aleff campos <aleff.mc@sempreceub.com>
2025-11-19 17:30:30 -0300

    organizacao dos arquivos do repo

---
+++ evosystem-backend/Core/WmiHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evosystem_backend.Core
+{
+    internal class WmiHelper
+    {
+    }
+}
